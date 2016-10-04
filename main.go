5986a5b8f6ca99d9f3cfc1ede1d9c3decccc0cf5
Sandip Southekal <sandip@ziprecruiter.com>
2016-10-04 18:52:28 -0500

    - added new route to handle access to meetup-api - updated README to indicate addition of meetup api service

--- main.go
+++ main.go
@@ -31,6 +31,24 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		api_url := "http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=devict&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=73273692&sig=9cdd3af6b5a26eb664fe5abab6e5cf7bfaaf090e"
+		res, err := http.Get(api_url)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer res.Body.Close()
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+
+		if _, err := io.Copy(w, res.Body); err != nil {
+			log.Println(err)
+		}
+	})
+
 	log.Println("Listening to", p)
 	log.Fatal(http.ListenAndServe(p, nil))
 }
